50cd9a908d6a947e20995ec2887e91ab40715a59
Jacob Roberts <jacobo0421@gmail.com>
2022-11-14 03:32:44 -0500

    Cleaned up Program.cs

--- Program.cs
+++ Program.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Text.Json;
-using System.Text.Json.Serialization;
-using System.Xml;
-using System.Xml.Linq;
-using DnDLike_Simulator.Classes;
 using DnDLike_Simulator.Initilization;
 
 string filePath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
@@ -16,32 +11,3 @@ ConfigInitialization.StartInitialization();
 
 WeaponInitialization.CheckForWeaponsList();
 
-// Maybe Save State via JSON in documentsFolder
-// Add Config file for where filepaths have been set
-//Check if File Exists, if not then Add
-
-// Weapon
-    // Weapon Damage
-    // Weapon Damage Type
-    // isRanged
-// Spell/Attack
-    // DamageMin
-    // DamageMax 
-    // Damage Type
-    // Description
-    // Move Type (Action, Bonus Action)
-    // RequiresSavingThrow?
-    // SavingThrowType
-    // AddSpell
-    // Advantage/Normal/Disadvantage
-// Character
-    // Character Class
-    // Character Level
-    // Hit Points
-    // Armor Class
-    // Primary Stats (Strength, Dex, Consitution, Intelligence, Wisdom, Charisma)
-// Battle
-    // Character
-    // Character Initiative
-    // Move Order
-    // IsAlive
